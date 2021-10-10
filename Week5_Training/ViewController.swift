5bc7e453931b0fe25b9fb5a62446e649dbb62e2a
kimjunsoo <rlawnstn01023@naver.com>
2021-10-10 14:27:43 +0900

    Initial Commit

---
+++ Week5_Training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week5_Training
+//
+//  Created by 준수김 on 2021/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
